79fa9e57d840ba9ff77b2b442aa19ebc7eeaa93f
ticticboooomer <kyle.benjamin.cox@outlook.com>
2023-10-03 05:29:13 +0100

    added telescope and friends

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -2,3 +2,4 @@ local opts = {noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>f", ":lua vim.lsp.buf.format({async = false})<CR>", opts)
